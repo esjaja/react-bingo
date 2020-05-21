d8bb0ac088dba669c1e4ed043b4e09ade8c1c4c5
esjaja <emmas191@gmail.com>
2020-05-21 18:39:06 +0800

    Updates

---
+++ precache-manifest.08cc8cb087ed0a4f03f4a6f1f2a1b0f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f7a92ab0d05a6205b86df282245640",
+    "url": "/react-bingo/index.html"
+  },
+  {
+    "revision": "07b61fedf6c93ccffc7e",
+    "url": "/react-bingo/static/css/main.2a509561.chunk.css"
+  },
+  {
+    "revision": "f7359c4459da6694b7d6",
+    "url": "/react-bingo/static/js/2.f15c1e69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-bingo/static/js/2.f15c1e69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07b61fedf6c93ccffc7e",
+    "url": "/react-bingo/static/js/main.ff92d1eb.chunk.js"
+  },
+  {
+    "revision": "01537f681abf2785b4fa",
+    "url": "/react-bingo/static/js/runtime-main.ef34652b.js"
+  }
+]);
